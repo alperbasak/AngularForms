b3b66a730ef71a10007d32e039a609df8ed19e72
alperbasak <aalperbasak@gmail.com>
2021-08-19 15:38:43 +0200

    grouping form elements with ngModelGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import {NgForm} from '@angular/forms';
 export class AppComponent {
   @ViewChild('formElement') form: NgForm;
   defaultQuestion = 'pet';
+  answer = '';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
